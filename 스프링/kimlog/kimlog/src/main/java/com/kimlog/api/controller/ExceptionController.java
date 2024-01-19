81e7b6ca5c45b1f3f244e30e212f8107b85490a4
kimh7537 <kimh7537@gmail.com>
2024-01-19 18:43:00 +0900

    update

---
+++ 스프링/kimlog/kimlog/src/main/java/com/kimlog/api/controller/ExceptionController.java
@@ -0,0 +1,12 @@
+package com.kimlog.api.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ExceptionController {
+    
+    public void exceptionHandler(){
+
+    }
+}
